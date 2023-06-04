76359246ba87a31d3b0627925cfc9c0c1862fc5a
Alexandr-Gusev <34835129+Alexandr-Gusev@users.noreply.github.com>
2023-06-04 10:59:48 +0300

    Implement filter

--- controller/books.controller.js
+++ controller/books.controller.js
@@ -16,11 +16,54 @@ class BooksController {
     async getBooks(req, res) {
         try {
             const newOnly = req.query.newOnly === "true"
-            const [books] = await db.query(
-                newOnly
-                    ? 'SELECT * FROM books ORDER BY book_id DESC LIMIT 4'
-                    : 'SELECT * FROM books ORDER BY title'
-            )
+            const filter = req.query.filter || {};
+
+            const conditions = [];
+            const values = [];
+
+            if (filter.query !== undefined && filter.query !== "") {
+                conditions.push('b.title LIKE ?');
+                values.push(`%${filter.query}%`);
+            }
+
+            if (filter.minPrice !== undefined) {
+                conditions.push('b.price >= ?');
+                values.push(parseInt(filter.minPrice));
+            }
+
+            if (filter.maxPrice !== undefined) {
+                conditions.push('b.price <= ?');
+                values.push(parseInt(filter.maxPrice));
+            }
+
+            if (filter.author !== undefined && filter.author !== '') {
+                conditions.push('b.author = ?');
+                values.push(filter.author);
+            }
+
+            if (filter.genre !== undefined) {
+                conditions.push('g.genre_id = ?');
+                values.push(filter.genre);
+            }
+
+            if (filter.publishment !== undefined) {
+                conditions.push('p.publishment_id = ?');
+                values.push(filter.publishment);
+            }
+
+            if (!conditions.length) {
+                conditions.push("TRUE IS ?")
+                values.push(true)
+            }
+
+            const sql = `SELECT b.*, g.*, p.* FROM books AS b
+                LEFT JOIN books_genres AS bg ON bg.book_id = b.book_id
+                LEFT JOIN genres AS g ON g.genre_id = bg.genre_id
+                LEFT JOIN books_publishments AS bp ON bp.book_id = b.book_id
+                LEFT JOIN publishments AS p ON p.publishment_id = bp.publishment_id
+                WHERE ${conditions.join(" AND ")} ORDER BY ` + (newOnly ? "book_id DESC LIMIT 4" : "title")
+
+            const [books] = await db.query(sql, values)
             res.json(books.map(x => x))
         } catch (error) {
             console.error(error)
@@ -41,29 +84,51 @@ class BooksController {
         }
     }
 
-    async getFilterOptions(req, res) {
+    async getPriceRange(req, res) {
         try {
             const [priceRange] = await db.query(
                 'SELECT MIN(price) AS min_price, MAX(price) AS max_price FROM books'
             )
-            const [authors] = await db.query(
-                'SELECT DISTINCT(author) FROM books ORDER BY author'
+            res.json([priceRange[0].min_price, priceRange[0].max_price])
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ error: 'Unexpected error' })
+        }
+    }
+
+    async getAuthors(req, res) {
+        try {
+            const [items] = await db.query(
+                'SELECT DISTINCT(author) FROM books WHERE author LIKE ? ORDER BY author LIMIT 10',
+                `%${req.query.name}%`
             )
-            const [genres] = await db.query(
-                'SELECT * FROM genres ORDER BY genre_name'
+            res.json(items.map(x => x.author))
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ error: 'Unexpected error' })
+        }
+    }
+
+    async getGenres(req, res) {
+        try {
+            const [items] = await db.query(
+                'SELECT * FROM genres WHERE genre_name LIKE ? ORDER BY genre_name LIMIT 10',
+                `%${req.query.name}%`
             )
-            const [publishments] = await db.query(
-                'SELECT * FROM publishments ORDER BY publishment_name'
+            res.json(items.map(x => x))
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ error: 'Unexpected error' })
+        }
+    }
+
+    async getPublishments(req, res) {
+        try {
+            const [items] = await db.query(
+                'SELECT * FROM publishments WHERE publishment_name LIKE ? ORDER BY publishment_name LIMIT 10',
+                `%${req.query.name}%`
             )
-            res.json({
-                price: {
-                    min: priceRange[0].min_price,
-                    max: priceRange[0].max_price
-                },
-                authors: authors.map(x => x),
-                genres: genres.map(x => x),
-                publishments: publishments.map(x => x)
-            })
+            res.json(items.map(x => x))
         } catch (error) {
             console.error(error)
             res.status(500).json({ error: 'Unexpected error' })
--- routes/books.routes.js
+++ routes/books.routes.js
@@ -5,6 +5,9 @@ const booksController = require ('../controller/books.controller')
 router.get('/books', booksController.getBooks)
 router.get('/books/:id', booksController.getOneBook)
 
-router.get('/filterOptions', booksController.getFilterOptions)
+router.get('/priceRange', booksController.getPriceRange)
+router.get('/authors', booksController.getAuthors)
+router.get('/genres', booksController.getGenres)
+router.get('/publishments', booksController.getPublishments)
 
 module.exports = router
